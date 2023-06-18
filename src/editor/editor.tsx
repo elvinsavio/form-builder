8e826c50b30d505b03c2db661cdea2dc7b8b8036
elvinsavio <elvinsavio.lp@gmail.com>
2023-06-18 21:02:42 +0530

    separate the header title and description

--- src/editor/editor.tsx
+++ src/editor/editor.tsx
@@ -7,6 +7,7 @@ import { editorStyle } from "../style/editor";
 import Input from "../components/input";
 import { IEditorStyle } from "../style/editor.type";
 import { Section } from "./section/section";
+import SectionHeader from "./section/sectionHeader";
 
 interface IProps {
   state: IForm;
@@ -28,6 +29,7 @@ export default function Editor({ state, setState, styles }: IProps) {
     tempState.sections.push({
       id: genUUID(),
       title: "New Section",
+      editTitle: false,
       fields: [],
       open: true,
     });
@@ -68,47 +70,37 @@ export default function Editor({ state, setState, styles }: IProps) {
 
   return (
     <div className={styles?.editor?.styles?.className} style={styles?.editor.styles?.style}>
-      <header style={styles?.header?.main?.styles?.style} className={styles?.header?.main?.styles?.className}>
-        {state?.editTitle ? (
-          <Input value={state.title} onSave={handleTitleSave} onCancel={toggleTitleEdit} />
-        ) : (
-          <>
-            {state?.title}
-            <Icon
-              onClick={toggleTitleEdit}
-              className={editorStyle?.icons?.edit?.styles?.className}
-              icon={<FiEdit2 />}
-            />
-          </>
-        )}
-      </header>
+      <EditorHeader
+        styles={styles}
+        edit={state?.editTitle}
+        title={state?.title}
+        toggleTitleEdit={toggleTitleEdit}
+        handleTitleSave={handleTitleSave}
+      />
 
-      <small style={styles?.header?.sub?.styles?.style} className={styles?.header?.sub?.styles?.className}>
-        {state?.editDescription ? (
-          <Input value={state?.description} onSave={handleDescriptionSave} onCancel={toggleDescriptionEdit} />
-        ) : (
-          <>
-            {state?.description}
-            <Icon
-              onClick={toggleDescriptionEdit}
-              className={editorStyle?.icons?.edit?.styles?.className}
-              icon={<FiEdit2 />}
-            />
-          </>
-        )}
-      </small>
+      <EditorSubHeader
+        styles={styles}
+        edit={state?.editDescription}
+        description={state?.description}
+        toggleDescriptionEdit={toggleDescriptionEdit}
+        handleDescriptionSave={handleDescriptionSave}
+      />
+
       <Section
         items={state.sections}
         onChange={handlesSectionChange}
-        renderItem={(item) => (
+        renderItem={(item, index) => (
           <Section.Item id={item.id}>
-            <div>
-              <div className="flex justify-between">
-                {item.title}
-                <Section.DragHandle />
-              </div>
-              hello world
-            </div>
+            <Section.Header
+              title={item?.title}
+              edit={item?.editTitle}
+              onDelete={() => 3}
+              onSave={() => 3}
+              toggleEdit={() => 5}
+            >
+              <Section.DragHandle />
+            </Section.Header>
+            Hello
           </Section.Item>
         )}
       />
@@ -132,32 +124,56 @@ function AddNewSection({ onClick }: { onClick: () => void }) {
   );
 }
 
-// {state.sections?.map((section, index) => (
-//   <SectionContainer key={index}>
-//     <SectionHeader>
-//       {index + 1} {section.title} -{" "}
-//     </SectionHeader>
-//     <small className="p-1 text-gray-300">
-//       {section.description}- {section.fields?.length} fields
-//     </small>
-//   </SectionContainer>
-// ))}
-// function SectionContainer({ children }: { children: ReactNode }) {
-//   return (
-//     <div className="flex flex-col gap-2  border border-slate-600 hover:border-slate-500 p-1 rounded transition-all">
-//       {children}
-//     </div>
-//   );
-// }
+interface IEditorHeader {
+  styles: IEditorStyle;
+  edit: boolean;
+  title: string;
+  toggleTitleEdit: () => void;
+  handleTitleSave: (value: string) => void;
+}
+function EditorHeader({ styles, edit, title, toggleTitleEdit, handleTitleSave }: IEditorHeader) {
+  return (
+    <header style={styles?.header?.main?.styles?.style} className={styles?.header?.main?.styles?.className}>
+      {edit ? (
+        <Input value={title} onSave={handleTitleSave} onCancel={toggleTitleEdit} />
+      ) : (
+        <>
+          {title}
+          <Icon onClick={toggleTitleEdit} className={editorStyle?.icons?.edit?.styles?.className} icon={<FiEdit2 />} />
+        </>
+      )}
+    </header>
+  );
+}
 
-// function SectionHeader({ children }: { children: ReactNode }) {
-//   return (
-//     <div className="flex items-center justify-between ">
-//       <div className="flex items-center gap-1">
-//         <MdOutlineExpandMore className="text-2xl hover:text-slate-100 hover:bg-slate-500 hover:bg-opacity-100 rounded cursor-pointer" />
-//         <header className="text">{children} </header>
-//       </div>
-//       hello
-//     </div>
-//   );
-// }
+interface IEditorSubHeader {
+  styles: IEditorStyle;
+  edit: boolean;
+  description: string;
+  toggleDescriptionEdit: () => void;
+  handleDescriptionSave: (value: string) => void;
+}
+function EditorSubHeader({
+  styles,
+  edit,
+  description,
+  toggleDescriptionEdit,
+  handleDescriptionSave,
+}: IEditorSubHeader) {
+  return (
+    <small style={styles?.header?.sub?.styles?.style} className={styles?.header?.sub?.styles?.className}>
+      {edit ? (
+        <Input value={description} onSave={handleDescriptionSave} onCancel={toggleDescriptionEdit} />
+      ) : (
+        <>
+          {description}
+          <Icon
+            onClick={toggleDescriptionEdit}
+            className={editorStyle?.icons?.edit?.styles?.className}
+            icon={<FiEdit2 />}
+          />
+        </>
+      )}
+    </small>
+  );
+}
